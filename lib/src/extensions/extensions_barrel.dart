780c1660eae6d3ea575836a42dd968eb85f44da5
Arindam Karmakar <connect@karindam.in>
2026-02-12 00:15:28 +0530

    Container util extension added

--- lib/src/extensions/extensions_barrel.dart
+++ lib/src/extensions/extensions_barrel.dart
@@ -1,8 +1,7 @@
 export 'rp_row_extension.dart';
 export 'rp_column_extension.dart';
 export 'rp_stack_extension.dart';
+export 'rp_container_extension.dart';
 
 export 'rp_text_extension.dart';
 export 'rp_image_extension.dart';
-
-export 'rp_widget_extension.dart';
